4aca35ade2735d6d27ea4a099e2cd24eed17aae0
kvojps <joseferreira.santosjunior@upe.br>
2022-07-22 14:17:10 -0300

    adicionando exercicio 4 - primeiro modulo

---
+++ componente1/modulo2/exe03.py
@@ -0,0 +1,23 @@
+def calculadora(x, y, operacao):
+    if operacao == '+' :
+        return soma(x,y)
+    elif operacao == '-':
+        return subtracao(x,y)
+    elif operacao == '*':
+        return multiplicacao(x,y)
+    else:
+        return divisao(x,y)
+
+def soma(x,y):
+    return x + y
+
+def subtracao(x, y):
+    return x - y
+
+def multiplicacao(x, y):
+    return x * y
+
+def divisao(x, y):
+    if y != 0:
+        return x / y
+    return "Não é possível dividir por 0."
