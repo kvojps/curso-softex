b9279af7ff13cadb67606ea7639d4c2fb1f497b8
kvojps <joseferreira.santosjunior@upe.br>
2022-07-23 11:13:18 -0300

    adicionando exercicio 06 do modulo 2[

---
+++ componente1/modulo2/exe05.py
@@ -0,0 +1,14 @@
+import datetime
+
+data  = datetime.date.today()
+anoAtual = int(data.strftime("%Y"))
+
+while True:
+    nome = input("Insira seu nome completo: ")
+    anoNascimento = int(input("Insira o ano do seu nascimento: "))
+
+    if anoNascimento >= 1922 and anoNascimento <= anoAtual - 1:
+        print(f"{nome} tem/terá {anoAtual - anoNascimento} anos em {anoAtual}!" )
+        break
+    else:
+        print("Preencha os dados corretamente!")
