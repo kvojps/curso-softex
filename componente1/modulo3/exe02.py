ef8ac6d07dfdec388c736a38124ef31926ce2a5b
kvojps <joseferreira.santosjunior@upe.br>
2022-07-24 17:49:04 -0300

    Adicionando exercicio 3 - modulo 3

---
+++ componente1/modulo3/exe02.py
@@ -0,0 +1,18 @@
+class NumeroComplexo:
+    def getReal(expr):
+        return expr.real
+
+    def getImag(expr):
+        return expr.imag
+
+    def somaComplex(expr0, expr1):
+        return expr0 + expr1
+    
+    def subtracaoComplex(expr0, expr1):
+        return expr0 - expr1
+
+    def multiplicacaoComplex(expr0, expr1):
+        return expr0 * expr1
+
+    def divisaoComplex(expr0, expr1):
+        return expr0 / expr1
