8b3239643f9ca1c45f5707b9b439bd9c1656f754
kvojps <joseferreira.santosjunior@upe.br>
2022-07-31 16:10:48 -0300

    adicionando exercicio 02 - modulo 4

---
+++ componente1/modulo4/exe02.py
@@ -0,0 +1,16 @@
+class Pessoa:
+    def __init__(self, nome, anoNascimento):
+        self.nome = nome
+        self.anoNascimento = anoNascimento
+
+    def getNome(self):
+        return self.nome
+
+    def setNome(self, nome):
+        self.nome = nome
+
+    def getAnoNascimento(self, anoNascimento):
+        self.anoNascimento = anoNascimento
+
+    def setAnoNascimento(self, anoNascimento):
+        self.anoNascimento = anoNascimento
